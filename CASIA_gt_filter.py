bf0f404d7ed3e2474f939030fbcc724d057760b0
Chieh-Jen Wang <cjwang0318@gmail.com>
2023-03-07 13:56:15 +0800

    update CASIA_gt_filter

--- CASIA_gt_filter.py
+++ CASIA_gt_filter.py
@@ -1,32 +1,37 @@
 import os
 from tqdm import tqdm
 
+
 def read_file(path, skip_lines_num):
     with open(path, 'r', encoding='utf-8') as f:
         lines = [line.strip() for line in f.readlines()]
     lines = lines[skip_lines_num:]
     return lines
 
+
 def write_file(path, write_data):
     with open(path, 'w', encoding='utf-8') as writer:
         writer.writelines(write_data)
 
+
 def get_unicode(str):
     res = ''.join(r'\u{:04X}'.format(ord(chr)) for chr in str)
 
+
 if __name__ == '__main__':
     # load paddle chinese dictionary
-    cht_dict=read_file("./paddle/chinese_cht_dict.txt", 0)
-    gt_list=read_file("./output/gt.txt", 0)
-    output_list=[]
+    root_path = "D:/temp/Gnt1.2Test"
+    cht_dict = read_file("./paddle/chinese_cht_dict.txt", 0)
+    gt_list = read_file(root_path + "/gt.txt", 0)
+    output_list = []
     for line in tqdm(gt_list):
-        temp=line.split('\t')
-        path=temp[0]
-        gt_word=temp[1]
+        temp = line.split('\t')
+        path = temp[0]
+        gt_word = temp[1]
         if gt_word in cht_dict:
-            #print(gt_word)
-            output_list.append(line+"\n")
+            # print(gt_word)
+            output_list.append(line + "\n")
         else:
-            #print("./output/images/"+path)
-            os.remove("./output/images/"+path)
-    write_file("./output/gt_filtered.txt", output_list)
+            # print("./output/images/"+path)
+            os.remove(root_path + "/images/" + path)
+    write_file(root_path + "/gt_filtered.txt", output_list)
