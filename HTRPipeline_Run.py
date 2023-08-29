50a8eeeb8b755bcd896a3a8d2678dfcd2beb7f9d
cjwang <cjwang0318@gmail.com>
2023-08-29 20:58:59 +0800

    update HTRPipeline

--- HTRPipeline_Run.py
+++ HTRPipeline_Run.py
@@ -1,8 +1,11 @@
 import cv2
 import os
+import numpy as np
 from htr_pipeline import read_page, DetectorConfig, LineClusteringConfig
 
-filepath = "./img/4.jpg"
+
+
+filepath = "./img/1.jpg"
 basename = os.path.basename(filepath)  # example.py
 filename = os.path.splitext(basename)[0]  # example
 
@@ -16,7 +19,8 @@ height, width = img2.shape[:2]  # image height and width
 print(f"image height={height}")
 
 # calculate scale value
-scaleValue = 1.76
+scaleValue = 1.24
+scaleValue=1
 print(f"scaleValue={scaleValue}")
 
 # detect and read text
@@ -30,15 +34,17 @@ read_lines = read_page(img, DetectorConfig(scale=scaleValue, margin=5),
 # print(read_lines)
 font = cv2.FONT_HERSHEY_SIMPLEX
 for i, read_line in enumerate(read_lines):
-    print("Number of BBX=" + str(len(read_line)))
     j=0
+    print("Number of BBX=" + str(len(read_line)))
     for read_word in read_line:
         aabb = read_word.aabb
         cv2.rectangle(img2, (aabb.xmin, aabb.ymin), (aabb.xmax, aabb.ymax), (0, 255, 0), 4)
-        img3 = img[aabb.ymin:aabb.ymax, aabb.xmin:aabb.xmax]
-        cv2.imwrite(f'./img/crop_img/{filename}_{j}.jpg', img3)
+        img4 = img3[aabb.ymin:aabb.ymax, aabb.xmin:aabb.xmax]
+        cv2.imwrite(f'./img/crop_img/{filename}_{j}.jpg', img4)
         j+=1
-        # text = read_word.text
+        line_detection(img4)
+        #text = read_word.text
+        #print(f"BBX_{j} Texe={text}")
     # cv2.putText(img2, text, (aabb.xmax, aabb.ymin), font, 1, (255, 0, 0), 1)
 
 cv2.imwrite('./BBX_result.jpg', img2)
